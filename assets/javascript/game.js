89eacba8e6c902147a98336c0516344b5d2513d9
gkwazy <noreply>
2018-05-11 20:01:13 -0600

    battling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,29 +52,66 @@ var waitingPokemonBottom = squirtle;
 var enemyFightingPokemon = enemyChamander;
 var waitingEnemyPokemonTop = enemyBulbasaur;
 var waitingEnemyPokemonBottom = enemySquirtle;
+var gamerunning = true;
 
-$("#topPokemon").on("click", function () {
-    change("top");
-});
+while (gamerunning) {
+    Battling();
+}
 
-$("#bottomPokemon").on("click", function () {
-    change("bottom");
-});
+$('#myModal').modal({ show: true });
 
-$("#attack").on("click", function () {
-    attack(fightingPokemon, enemyFightingPokemon, 1);
-    computerAction(enemyFightingPokemon, fightingPokemon);
 
-});
+function Battling() {
+    $("#topPokemon").on("click", function () {
+        change("top");
+    });
+
+    $("#bottomPokemon").on("click", function () {
+        change("bottom");
+    });
+
+    $("#attack").on("click", function () {
+        attack(fightingPokemon, enemyFightingPokemon, 1);
+        computerAction(enemyFightingPokemon, fightingPokemon);
+        if (fightingPokemon.health < 0) {
+            fightingPokemon.living = false;
+            fightingPokemon.img = "assets/images/dead.png"
+            if (waitingPokemonTop.living == true) {
+                change("top")
+            }
+            else if (waitingPokemonBottom.living == true) {
+                change("bottom")
+            }
+        }
+        if (enemyFightingPokemon.health < 0) {
+            enemyFightingPokemon.living = false;
+            enemyFightingPokemon.img = "assets/images/dead.png"
+            if (waitingEnemyPokemonTop.living == true) {
+                changeEnemy("top")
+            }
+            else if (waitingEnemyPokemonBottom.living == true) {
+                changeEnemy("bottom")
+            }
+        }
+
+    });
+    if (!fightingPokemon.living && !waitingPokemonBottom.living && !waitingEnemyPokemonTop.living
+        || !enemyFightingPokemon.living && !waitingEnemyPokemonBottom && !waitingEnemyPokemonTop) {
+        gamerunning = false;
+    }
+}
+
+
+
 
 function computerAction(enemy, player) {
     var choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0: attack(enemyFightingPokemon, fightingPokemon, 2)
             break;
-        case 1: changeEnemy();
+        case 1: attack(enemyFightingPokemon, fightingPokemon, 2)
             break;
-        case 2: changeEnemy();
+        case 2: attack(enemyFightingPokemon, fightingPokemon, 2)
             break;
     }
 }
@@ -101,25 +138,31 @@ function attack(attackingPokemon, defendingPokemon, personAttacking) {
 }
 
 function change(position) {
-    console.log("methond called")
+    console.log("method called")
     if (position == "top") {
         var tempPokemon;
-        tempPokemon = fightingPokemon;
-        fightingPokemon = waitingPokemonTop;
-        waitingPokemonTop = tempPokemon;
-        document.getElementById("fightingPokemon").src = fightingPokemon.img;
-        document.getElementById("topPokemon").src = waitingPokemonTop.img;
-        console.log("top")
+        if (waitingPokemonTop.health > 0) {
+            tempPokemon = fightingPokemon;
+            fightingPokemon = waitingPokemonTop;
+            waitingPokemonTop = tempPokemon;
+            document.getElementById("fightingPokemon").src = fightingPokemon.img;
+            document.getElementById("topPokemon").src = waitingPokemonTop.img;
+            console.log("top")
+        }
+        else (alert("You can't tade in a defeated Pokemon"))
     }
     else if (position == "bottom") {
         var tempPokemon;
-        tempPokemon = fightingPokemon;
-        fightingPokemon = waitingPokemonBottom;
-        waitingPokemonBottom = tempPokemon;
-        console.log(fightingPokemon + " " + tempPokemon + " " + waitingPokemonBottom)
-        document.getElementById("fightingPokemon").src = fightingPokemon.img;
-        document.getElementById("bottomPokemon").src = waitingPokemonBottom.img;
-        console.log("bot")
+        if (waitingPokemonBottom.health > 0) {
+            tempPokemon = fightingPokemon;
+            fightingPokemon = waitingPokemonBottom;
+            waitingPokemonBottom = tempPokemon;
+            console.log(fightingPokemon + " " + tempPokemon + " " + waitingPokemonBottom)
+            document.getElementById("fightingPokemon").src = fightingPokemon.img;
+            document.getElementById("bottomPokemon").src = waitingPokemonBottom.img;
+            console.log("bot")
+        }
+        else (alert("You can't tade in a defeated Pokemon"))
     }
     else {
         alert("error!!!!!!!!!!!!!!");
@@ -135,32 +178,40 @@ function change(position) {
     console.log("your dude " + fightingPokemon.health);
     console.log(document.getElementById("yourHealth").style.width);
 }
+
+
 function changeEnemy() {
     console.log("methond called")
     var computerSwitch = Math.floor(Math.random() * 2);
 
     if (computerSwitch == 0) {
         var tempPokemon;
-        tempPokemon = enemyFightingPokemon;
-        enemyFightingPokemon = waitingEnemyPokemonTop;
-        waitingEnemyPokemonTop = tempPokemon;
-        document.getElementById("enemyFightingPokemon").src = enemyFightingPokemon.img;
-        document.getElementById("waitingEnemyPokemonTop").src = waitingEnemyPokemonTop.img;
-        console.log("waitingEnemyPokemonTop")
+        if (waitingEnemyPokemonTop.health > 0) {
+            tempPokemon = enemyFightingPokemon;
+            enemyFightingPokemon = waitingEnemyPokemonTop;
+            waitingEnemyPokemonTop = tempPokemon;
+            document.getElementById("enemyFightingPokemon").src = enemyFightingPokemon.img;
+            document.getElementById("waitingEnemyPokemonTop").src = waitingEnemyPokemonTop.img;
+            console.log("waitingEnemyPokemonTop")
+        }
+        else (alert("You can't tade in a defeated Pokemon"))
     }
     else if (computerSwitch == 1) {
         var tempPokemon;
-        tempPokemon = enemyFightingPokemon;
-        enemyFightingPokemon = waitingEnemyPokemonBottom;
-        waitingEnemyPokemonBottom = tempPokemon;
-        console.log(enemyFightingPokemon + " " + tempPokemon + " " + waitingPokemonBottom)
-        document.getElementById("enemyFightingPokemon").src = enemyFightingPokemon.img;
-        document.getElementById("waitingEnemyPokemonBottom").src = waitingEnemyPokemonBottom.img;
-        console.log("fdafd bot")
+        if (waitingEnemyPokemonBottom.health > 0) {
+            tempPokemon = enemyFightingPokemon;
+            enemyFightingPokemon = waitingEnemyPokemonBottom;
+            waitingEnemyPokemonBottom = tempPokemon;
+            console.log(enemyFightingPokemon + " " + tempPokemon + " " + waitingPokemonBottom)
+            document.getElementById("enemyFightingPokemon").src = enemyFightingPokemon.img;
+            document.getElementById("waitingEnemyPokemonBottom").src = waitingEnemyPokemonBottom.img;
+            console.log("fdafd bot")
+        }
+
     }
 
-
-
+    var healthPercent = Math.round(100 * (enemyFightingPokemon.health / 120));
+    document.getElementById("enemyHealth").style.width = healthPercent + "%";
     console.log(fightingPokemon)
     console.log(tempPokemon)
     console.log(waitingPokemonBottom)
